d0c5660250cf2439febc67a8631f98079742a313
Esteban Ramirez <esteban.ramirez@playtravel.com.au>
2021-09-23 22:03:23 +1000

    init implementation

---
+++ samples/components/DE_Dropdown.mjs
@@ -0,0 +1,200 @@
+import Utils from "./Utils.mjs";
+
+class DE_Dropdown extends HTMLElement 
+{
+  static name = "de-dropdown";
+
+  constructor() 
+  {
+    super();
+
+    //this.Show = this.Show.bind(this);
+    //this.Hide = this.Hide.bind(this);
+    this.On_Click_Src_Elem = this.On_Click_Src_Elem.bind(this);
+    this.On_Click_Window = this.On_Click_Window.bind(this);
+    this.On_Scroll_Window = this.On_Scroll_Window.bind(this);
+
+    this.attachShadow({mode: 'open'});
+  }
+
+  connectedCallback()
+  {
+    this.Render();
+  }
+
+  static observedAttributes = ["src-elem"];
+  attributeChangedCallback(attrName, oldValue, newValue)
+  {
+    if (attrName == "src-elem")
+    {
+      this.src_elem_id = newValue;
+    }
+  }
+
+  set srcElem(elem)
+  {
+    this.Set_Src_Elem(elem);
+  }
+
+  Set_Src_Elem(elem)
+  {
+    this.src_elem = elem;
+    elem.addEventListener("click", this.On_Click_Src_Elem);
+  }
+
+  set items(items)
+  {
+    this.Set_Items(items);
+  }
+
+  Set_Items(items)
+  {
+    const item_elem = document.createElement("span");
+    item_elem.style.fontSize = "20px";
+    item_elem.style.fontWeight = "bold";
+    item_elem.style.padding = "0px 5px";
+    item_elem.style.textAlign = "right";
+    item_elem.style.backgroundColor = '#f2f2f2';
+    item_elem.addEventListener("click", this.On_Click_Src_Elem);
+    item_elem.innerHTML = "&#215;";
+    this.shadowRoot.append(item_elem);
+
+    for (const item of items)
+    {
+      const item_elem = document.createElement("span");
+      item_elem.item_data = item.data;
+      item_elem.classList.add("item");
+      item_elem.addEventListener("click", item.action);
+      item_elem.addEventListener('mouseup', this.On_Click_Src_Elem);
+      item_elem.append(item.label);
+
+      this.shadowRoot.append(item_elem);
+    }
+  }
+
+  On_Click_Window(event)
+  {
+    const elems = event.composedPath();
+    const is_menu_click = elems.includes(this);
+    if (!is_menu_click)
+    {
+      this.Hide();
+    }
+  }
+
+  On_Scroll_Window(event)
+  {
+    this.Hide();
+  }
+
+  On_Click_Src_Elem(event)
+  {
+    this.Toggle();
+    event.stopPropagation();
+  }
+
+  Show()
+  {
+    let x, y;
+
+    const padding = 10;
+    const thisWidth = this.Get_Width(200);
+    const docRect = window.document.body.getBoundingClientRect();
+    const rect = this.src_elem.getBoundingClientRect();
+    if (docRect.width - rect.left < thisWidth + padding)
+    {
+      x = docRect.width - thisWidth - padding;
+      y = rect.bottom;
+    }
+    else
+    {
+      x = rect.left;
+      y = rect.bottom;
+    }
+
+    if (!Utils.isEmpty(window.ptDropdowns))
+    {
+      for (const ptDropdown of window.ptDropdowns)
+      {
+        ptDropdown.Hide();
+      }
+    }
+
+    if (!Utils.hasValue(window.ptDropdowns))
+    {
+      window.ptDropdowns = [];
+    }
+    window.ptDropdowns.push(this);
+
+    this.style.left = x + "px";
+    this.style.top = y + "px";
+    this.style.display = "flex";
+    window.addEventListener("click", this.On_Click_Window);
+    window.addEventListener("scroll", this.On_Scroll_Window);
+    window.addEventListener("resize", this.On_Scroll_Window);
+  }
+
+  Get_Width(def)
+  {
+    let width = parseInt(this.style.width);
+    if (isNaN(width))
+    {
+      width = def;
+    }
+    return width;
+  }
+
+  Hide()
+  {
+    this.style.display = "none";
+    window.removeEventListener("click", this.On_Click_Window);
+    window.removeEventListener("scroll", this.On_Scroll_Window);
+    window.removeEventListener("resize", this.On_Scroll_Window);
+  }
+
+  Toggle()
+  {
+    if (this.style.display == "flex")
+    {
+      this.Hide();
+    }
+    else
+    {
+      this.Show();
+    }
+  }
+
+  Render()
+  {
+    this.style.display = "none";
+    this.style.flexDirection = "column";
+    this.style.cursor = "pointer";
+    this.style.position = "fixed";
+    this.style.border = "1px solid #ccc";
+    this.style.backgroundColor = "#fff";
+    this.style.boxShadow = 'grey 2px 2px 5px';
+    this.style.zIndex = '10';
+    
+    const styleElem = document.createElement("style");
+    styleElem.innerHTML = 
+      `.item 
+      {
+        font-size: 13px;
+        font-weight: normal;
+        padding: 5px 10px;
+      }
+      .item:hover
+      {
+        background-color: #eee;
+      }`;
+    this.shadowRoot.appendChild(styleElem);
+
+    if (this.src_elem_id)
+    {
+      const srcElem = document.getElementById(this.src_elem_id);
+      this.Set_Src_Elem(srcElem);
+    }
+  }
+}
+
+export default DE_Dropdown;
---
+++ samples/components/Filter_Buddy.mjs
@@ -0,0 +1,404 @@
+import Utils from "./Utils.mjs";
+import ptDropdown from "./DE_Dropdown.mjs"
+
+class ptFilter extends HTMLElement 
+{
+  static name = "filter-buddy";
+
+  // lifecycle ====================================================================================
+
+  constructor() 
+  {
+    super();
+    this.OnClick_Switch_View_Btn = this.OnClick_Switch_View_Btn.bind(this);
+    this.OnClick_Set_View = this.OnClick_Set_View.bind(this);
+    this.OnClick_Min_Add_Filter_Btn = this.OnClick_Min_Add_Filter_Btn.bind(this);
+    this.OnClick_Mid_Add_Filter_Btn = this.OnClick_Mid_Add_Filter_Btn.bind(this);
+    this.OnClick_Search_Btn = this.OnClick_Search_Btn.bind(this);
+    this.OnClick_Del_Filter_Btn = this.OnClick_Del_Filter_Btn.bind(this);
+  }
+
+  connectedCallback()
+  {
+    const rootElem = this.Render();
+    this.replaceChildren(rootElem);
+  }
+
+  disconnectedCallback()
+  {
+
+  }
+
+  adoptedCallback()
+  {
+
+  }
+
+  //static observedAttributes = ['a1', "a2", "a3"];
+  attributeChangedCallback(attrName, oldValue, newValue)
+  {
+
+  }
+
+  // fields =======================================================================================
+
+  // input types: text, time, time_range, date, date_range, timestamp, timestamp_range,
+  // boolean, one_off, integer, integer_range, float, float_range, currency, currency_range
+  // auto_complete
+  set filters(filter_defs)
+  {
+    /*
+      filterDefs =
+      [
+        {
+          id: "WHERE_SOURCE",
+          inputType: "text", 
+          in_mid_view: true
+        }
+      ]
+    */
+    this.filter_defs = filter_defs;
+    this.Render_View(this.mid_filter_defs, "mid_filters_div", "mid");
+    this.Render_View(this.filter_defs, "max_filters_div", "max");
+  }
+
+  set view(view_name)
+  {
+    const filter_data = this.Get_View_Data();
+    this.Show_View(view_name);
+    this.Set_View_Data(filter_data);
+
+    this.Render_Update_Summ();
+  }
+
+  get view()
+  {
+    let res;
+
+    if (!this.min_view_div.hidden)
+    {
+      res = "min";
+    }
+    else if (!this.mid_view_div.hidden)
+    {
+      res = "mid";
+    }
+    else if (!this.max_view_div.hidden)
+    {
+      res = "max";
+    }
+
+    return res;
+  }
+
+  get mid_filter_defs()
+  {
+    let res;
+
+    if (this.filter_defs)
+    {
+      res = this.filter_defs.filter(d => d.in_mid_view);
+    }
+
+    return res;
+  }
+
+  // events =======================================================================================
+
+  OnClick_Switch_View_Btn()
+  {
+
+  }
+
+  OnClick_Set_View(event)
+  {
+    this.view = event.target.item_data;
+  }
+
+  OnClick_Min_Add_Filter_Btn()
+  {
+    this.return_view = "min";
+    this.view = "max";
+  }
+
+  OnClick_Mid_Add_Filter_Btn()
+  {
+    this.return_view = "mid";
+    this.view = "max";
+  }
+
+  OnClick_Search_Btn()
+  {
+    const filter_data = this.Get_Data();
+    const search_event = new CustomEvent("search", {detail: filter_data});
+    this.dispatchEvent(search_event);
+
+    if (this.return_view)
+    {
+      this.view = this.return_view;
+      this.return_view = null;
+    }
+  }
+
+  OnClick_Del_Filter_Btn(def)
+  {
+    def.value = undefined;
+    this.Render_Update_Summ();
+  }
+
+  // misc =========================================================================================
+
+  Get_View_Data()
+  {
+    const view = this.view;
+
+    const defs = this.Get_Current_Defs();
+    if (!Utils.isEmpty(defs))
+    {
+      for (const def of defs)
+      {
+        const filter = def[view + "_filter"];
+        def.value = filter.value;
+      }
+    }
+  }
+
+  Set_View_Data()
+  {
+    const view = this.view;
+
+    const defs = this.Get_Current_Defs();
+    if (!Utils.isEmpty(defs))
+    {
+      for (const def of defs)
+      {
+        const filter = def[view + "_filter"];
+        filter.value = def.value;
+      }
+    }
+  }
+
+  Get_Data()
+  {
+    const res = {};
+
+    if (!Utils.isEmpty(this.filter_defs))
+    {
+      for (const def of this.filter_defs)
+      {
+        res[def.id] = def.value;
+      }
+    }
+
+    return res;
+  }
+
+  Get_Current_Defs()
+  {
+    let defs;
+
+    const view = this.view;
+    if (view == "mid")
+    {
+      defs = this.mid_filter_defs;
+    }
+    else if (view == "max")
+    {
+      defs = this.filter_defs;
+    }
+
+    return defs;
+  }
+
+  New_Filter(def)
+  {
+    let filter;
+
+    if (def.class_name == "Filter_Text")
+    {
+      filter = new Filter_Text(def);
+    }
+
+    return filter;
+  }
+
+  // rendering ====================================================================================
+
+  Show_View(view_name)
+  {
+    this.min_view_div.hidden = true;
+    this.mid_view_div.hidden = true;
+    this.max_view_div.hidden = true;
+    this[view_name + "_view_div"].hidden = false;
+  }
+
+  Render_Update_Summ()
+  {
+    const view = this.view;
+    const summ_div = this.querySelector("#" + view + "_summ_div");
+    if (summ_div)
+    {
+      const summary_elems = [];
+
+      let defs = this.filter_defs;
+      if (view == "mid")
+      {
+        defs = this.filter_defs.filter(d => !d.in_mid_view);
+      }
+
+      for (const def of defs)
+      {
+        const summary_elem = this.Render_Summary_Elem(def);
+        if (summary_elem)
+        {
+          summary_elems.push(summary_elem);
+        }
+      }
+      summ_div.replaceChildren(...summary_elems);
+    }
+  }
+
+  Render_Summary_Elem(def)
+  {
+    let span;
+
+    const value = def.value;
+    if (value)
+    {
+      const delete_btn = document.createElement("button");
+      delete_btn.addEventListener("click", () => this.OnClick_Del_Filter_Btn(def));
+      delete_btn.innerText = "Delete";
+
+      span = document.createElement("span");
+      span.innerText = def.label + " = " + value;
+      span.style.border = "1px solid red";
+      span.append(delete_btn);
+    }
+
+    return span;
+  }
+
+  Render_View(filter_defs, filters_div_id, view)
+  {
+    if (!Utils.isEmpty(filter_defs))
+    {
+      const elems = [];
+      for (const filter_def of filter_defs)
+      {
+        const filter = this.New_Filter(filter_def);
+        const filter_elems = filter.Render();
+        elems.push(filter_elems);
+
+        filter_def[view + "_filter"] = filter;
+      }
+      
+      const filters_div = this.querySelector("#" + filters_div_id);
+      filters_div.replaceChildren(...elems.flat());
+    }
+  }
+
+  Render_Filter_Status()
+  {
+
+  }
+
+  Render()
+  {
+    const html = `
+      <button id="switchViewBtn">view</button>
+      <span id="switchViewListPlaceholder"></span>
+      <button id="search_btn">Search</button>
+
+      <div id="min_view_div">
+        <button id="min_add_filter_btn">+</button>
+        <div id="min_summ_div"></div>
+      </div>
+
+      <div id="mid_view_div">
+        <button id="mid_add_filter_btn">+</button>
+        <div id="mid_filters_div"></div>
+        <div id="mid_summ_div"></div>
+      </div>
+
+      <div id="max_view_div">
+        <div id="max_filters_div"></div>
+      </div>
+    `;
+    const doc = Utils.toDocument(html);
+
+    const search_btn = doc.getElementById("search_btn");
+    search_btn.addEventListener("click", this.OnClick_Search_Btn);
+
+    const min_add_filter_btn = doc.getElementById("min_add_filter_btn");
+    min_add_filter_btn.addEventListener("click", this.OnClick_Min_Add_Filter_Btn);
+    const mid_add_filter_btn = doc.getElementById("mid_add_filter_btn");
+    mid_add_filter_btn.addEventListener("click", this.OnClick_Mid_Add_Filter_Btn);
+
+    this.min_view_div = doc.getElementById("min_view_div");
+    this.mid_view_div = doc.getElementById("mid_view_div");
+    this.max_view_div = doc.getElementById("max_view_div");
+    this.view = "min";
+
+    this.switchViewBtn = doc.getElementById("switchViewBtn");
+    this.switchViewBtn.addEventListener("click", this.OnClick_Switch_View_Btn);
+
+    //await window.customElements.whenDefined('pt-dropdown');
+    const items = 
+    [
+      {label: 'Close', action: this.OnClick_Set_View, data: "min"},
+      {label: 'Minimal', action: this.OnClick_Set_View, data: "mid"},
+      {label: 'Open', action: this.OnClick_Set_View, data: "max"},
+    ];
+    const switchViewList = new ptDropdown();
+    switchViewList.items = items;
+    switchViewList.srcElem = this.switchViewBtn;
+    switchViewList.style.width = "100px";
+    doc.getElementById("switchViewListPlaceholder").append(switchViewList);
+
+    return doc;
+  }
+}
+
+class Filter_Text
+{
+  constructor(def)
+  {
+    this.def = def;
+  }
+
+  set value(input_value)
+  {
+    this.input.value = "";
+    if (!Utils.isEmpty(input_value))
+    {
+      this.input.value = input_value;
+    }
+  }
+
+  get value()
+  {
+    let res;
+
+    const input_value = this.input.value;
+    if (!Utils.isEmpty(input_value))
+    {
+      res = input_value;
+    }
+
+    return res;
+  }
+
+  Render()
+  {
+    this.input = document.createElement("input");
+    this.input.id = "ptFilter_" + this.def.id;
+
+    this.label = document.createElement("label");
+    this.label.for = this.input.id;
+    this.label.innerText = this.def.label;
+
+    return [this.label, this.input];
+  }
+}
+
+export default ptFilter;
